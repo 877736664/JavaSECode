85808e51e80f3c47debb006a0e9d0e478018ba76
hz_sts_ys_zl <zhangle@siteshun.com>
2025-07-11 17:12:43 +0800

    1

---
+++ chapter11_api_teacher/src/com/atguigu01/string/StringDemo.java
@@ -0,0 +1,18 @@
+package com.atguigu01.string;
+import org.junit.Test;
+
+/**
+ * ClassName: StringDemo
+ * Description:
+ *
+ * @author Amingosazul
+ * @Create 2025/7/11 16:59
+ * @Version 1.0
+ */
+public class StringDemo {
+    @Test
+    public void test1() {
+        String s1 = "abc"; // 字面量的定义方式
+    }
+}
+
